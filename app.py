477dcaa5300c4bc2336a32f689f6a80e6e5c9b6c
kamr8431 <rishikamath09@gmail.com>
2025-08-11 18:49:47 -0700

    added loading text

--- app.py
+++ app.py
@@ -39,5 +39,7 @@ def download():
     return send_file(CHART_FILEPATH, mimetype='image/png', as_attachment=True, download_name='chart.png')
 
 if __name__ == '__main__':
+    #app.run(host='127.0.0.1', port=5000, debug=True)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+   
